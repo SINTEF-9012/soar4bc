4be09e36706c83d91f5fcd3af95366d9176432ee
Adela <adelanedisan_aniela@hotmail.com>
2024-01-10 15:56:21 +0100

    Updated chart files (static and analytics) to facilitate NLP solution.

--- src/chart/staticdata/StaticDataChart.tsx
+++ src/chart/staticdata/StaticDataChart.tsx
@@ -13,13 +13,13 @@ const StaticDataChart = (props: ChartProps) => {
   const { records, settings, getGlobalParameter } = props;
   const node = records && records[0] && records[0]._fields && records[0]._fields[0] ? records[0]._fields[0] : {};
   const [url, setUrl] = useState('');
-  const name = node.properties['endpoint']
+  const endpoint = node.properties['endpoint']
   const type = node.properties['type']
-  const nodename = name + '.' + type; // Add extension to name
+  const filename = endpoint + '.' + type; // Add extension to name
 
   const fetchUrl = () => {
     // Existing code to fetch the URL
-    const httpString = 'http://127.0.0.1:5000/get_minio_url?node_name=' + nodename;
+    const httpString = 'http://localhost:5000/minio_get_url?file_name=' + filename;
     axios.get(httpString)
       .then((response) => {
         const apiUrl = response.data.url;
@@ -33,39 +33,33 @@ const StaticDataChart = (props: ChartProps) => {
   useEffect(() => {
     // Fetch the URL when the component mounts
     fetchUrl();
-  }, [nodename]);
+  }, [endpoint]);
 
   useEffect(() => {
     const fetchAndUpdateUrl = async () => {
       try {
-        // Fetch the URL
-        const httpString = 'http://127.0.0.1:5000/get_minio_url?node_name=' + nodename;
-        const response = await axios.get(httpString);
-        const apiUrl = response.data.url;
-        
         // Update the URL in the Neo4J graph
-        if (apiUrl) {
-          await axios.post('http://127.0.0.1:5001/updateNode', {
-            node_name: name,  // Replace with actual node name
-            url: apiUrl
+        if (url) {
+          await axios.post('http://localhost:5001/update_url', {
+            node_endpoint: endpoint,  // Replace with actual node name
+            url: url
           });
-          console.log('Node updated with URL:', apiUrl);
+          console.log('Node updated with URL:', url);
         }
       } catch (error) {
         console.error('Error in fetchAndUpdateUrl:', error);
       }
     };
-    // TODO: Change according to timelapse:
-    if (nodename) {
+    // TODO: Change according to time expire date:
+    if (endpoint) {
       fetchAndUpdateUrl();
     }
-  }, [nodename]); // Dependency array - re-run when `nodename` changes
+  }, [url, endpoint]); // Re-run when `endpoint` and 'url' changes
 
   return ( 
     <div style={{ marginTop: '0px', height: '100%', textAlign: 'center'}}>
-      <p>Static data name: {name} </p>
-      <p>Static data type: {type} </p>
-      {url && <a href={url} target="_blank" rel="noopener noreferrer"><button style={{ fontSize: '16px', padding: '10px 20px' }}>Download</button></a>}
+      <p style={{ fontSize: '18px' }}> Click to download PCAP: </p>
+      {url && <a href={url} target="_blank" rel="noopener noreferrer"><button style={{ fontSize: '18px', padding: '10px 20px' }}>Download</button></a>}
     </div>
   );
 };
